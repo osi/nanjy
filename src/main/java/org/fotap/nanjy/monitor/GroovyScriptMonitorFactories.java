aa3722c4899cf2f69e6587a618bfa8cd187ac112
peter.royal@pobox.com <peter.royal@pobox.com@cff00c08-9bfb-11dd-9026-69e4e771c826>
2008-12-12 06:12:56 +0000

    Support specifying the MonitorFactories for a package via META-INF Since we cannot put code in java.lang, implement a mechanism to specify the MonitorFactories implementation to use via META-INF
    git-svn-id: https://nanjy.googlecode.com/svn/trunk@21 cff00c08-9bfb-11dd-9026-69e4e771c826

--- src/main/java/org/fotap/nanjy/monitor/GroovyScriptMonitorFactories.java
+++ src/main/java/org/fotap/nanjy/monitor/GroovyScriptMonitorFactories.java
@@ -1,6 +1,12 @@
 package org.fotap.nanjy.monitor;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.management.ObjectName;
 
 import org.slf4j.Logger;
@@ -12,6 +18,7 @@ import groovy.lang.GroovyClassLoader;
 public class GroovyScriptMonitorFactories implements MonitorFactories {
     private static final Logger logger = LoggerFactory.getLogger( GroovyScriptMonitorFactories.class );
 
+    private final Map<String, MonitorFactories> factoriesMap = new ConcurrentHashMap<String, MonitorFactories>();
     private final GroovyClassLoader loader;
 
     public GroovyScriptMonitorFactories( URL source ) {
@@ -28,36 +35,105 @@ public class GroovyScriptMonitorFactories implements MonitorFactories {
     }
 
     private MonitorFactories factoryForPackage( String pkg ) {
-        Class<?> clazz;
+        MonitorFactories factories = factoriesMap.get( pkg );
 
-        try {
-            clazz = loader.loadClass( pkg + ".MonitorFactories", true, false );
-        } catch( ClassNotFoundException e ) {
-            logger.trace( "No monitor for package: " + pkg, e );
+        if ( null != factories ) {
+            return factories;
+        }
 
-            int i = pkg.lastIndexOf( '.' );
+        Class<?> clazz = findFactoryClass( pkg );
 
-            if ( i > 0 ) {
-                return factoryForPackage( pkg.substring( 0, i ) );
-            }
+        if ( null == clazz ) {
+            return factoryForSuperPackage( pkg );
+        } else if ( !MonitorFactories.class.isAssignableFrom( clazz ) ) {
+            logger.warn( "Found MonitorFactories {}, but it isn't a {}",
+                         clazz.getName(),
+                         MonitorFactories.class.getName() );
 
             return null;
         }
 
-        if ( MonitorFactories.class.isAssignableFrom( clazz ) ) {
-            try {
-                return clazz.asSubclass( MonitorFactories.class ).newInstance();
-            } catch( Exception e ) {
-                logger.error( "Unable to create new instance of " + clazz.getName(), e );
+        try {
+            factories = clazz.asSubclass( MonitorFactories.class ).newInstance();
 
-                return null;
+            factoriesMap.put( pkg, factories );
+
+            return factories;
+        } catch( Exception e ) {
+            logger.error( "Unable to create new instance of " + clazz.getName(), e );
+
+            return null;
+        }
+    }
+
+    private Class<?> findFactoryClass( String pkg ) {
+        Class<?> clazz = findClassInPackage( pkg );
+
+        if ( null != clazz ) {
+            return clazz;
+        }
+
+        return findClassByResource( pkg );
+    }
+
+    private Class<?> findClassByResource( String pkg ) {
+        String resource = "META-INF/nanjy/" + pkg + "/MonitorFactories.properties";
+        InputStream stream = loader.getResourceAsStream( resource );
+
+        if ( null == stream ) {
+            logger.trace( "No resource declaration for package: {} - {}", pkg, resource );
+            return null;
+        }
+
+        LineNumberReader reader = null;
+        String className;
+
+        try {
+            reader = new LineNumberReader( new InputStreamReader( stream, "UTF-8" ) );
+            className = reader.readLine();
+        } catch( IOException e ) {
+            logger.warn( "Unable to read resource " + resource, e );
+            return null;
+        } finally {
+            if ( null != reader ) {
+                try {
+                    reader.close();
+                } catch( IOException e ) {
+                    logger.trace( "Unable to close resource reader", e );
+                }
             }
         }
 
-        logger.warn( "Found MonitorFactories {}, but it isn't a {}",
-                     clazz.getName(),
-                     MonitorFactories.class.getName() );
+        if ( null == className ) {
+            logger.warn( "Found empty resource declaration {}", resource );
+            return null;
+        }
+
+        try {
+            return loader.loadClass( className );
+        } catch( ClassNotFoundException e ) {
+            logger.warn( "Resource declaration " + resource + " specifies unknown class " + className, e );
+            return null;
+        }
+    }
+
+    private MonitorFactories factoryForSuperPackage( String pkg ) {
+        int i = pkg.lastIndexOf( '.' );
+
+        if ( i > 0 ) {
+            return factoryForPackage( pkg.substring( 0, i ) );
+        }
 
         return null;
+    }
+
+    private Class<?> findClassInPackage( String pkg ) {
+        try {
+            return loader.loadClass( pkg + ".MonitorFactories", true, false );
+        } catch( ClassNotFoundException e ) {
+            logger.trace( "No monitor class in package: " + pkg, e );
+
+            return null;
+        }
     }
 }
